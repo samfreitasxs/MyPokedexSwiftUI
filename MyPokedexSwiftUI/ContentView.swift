70edd5e4fe01a5166be7c9c3219b0064d3deef13
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-12-05 01:25:49 -0300

    feat: TELA DE ONBOARDING

--- MyPokedexSwiftUI/ContentView.swift
+++ MyPokedexSwiftUI/ContentView.swift
@@ -10,12 +10,86 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            trainersImages
+            Spacer().frame(height: 45)
+            
+            titleAndDescription
+            Spacer().frame(height: 24)
+            
+            onboardingProgressView
+            Spacer().frame(height: 24)
+            
+            continueButton
+            
         }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
+               maxHeight: .infinity,
+               alignment: .bottom)
         .padding()
+    }
+    
+    var trainersImages: some View {
+        // Utilizando a ZSstack para sobrepor as imagens.
+        ZStack {
+            Image("trainer1")
+            // Com o (.offset) pode se mudar os eixos com x e y.
+                .offset(x: -50)
+            //Background pode se colocar imagens atras.
+                .background {
+                    Image("shadowTrainer1")
+                        .offset(x: -65, y: 110)
+                }
+            Image("trainer2")
+                .offset(x: 50, y: -10)
+                .background{
+                    Image("shadowTrainer2")
+                        .offset(x: 50, y: 115)
+                }
+            // Eixo X e o orizontal e o Y e vertical.
+        }
+    }
+    
+    var titleAndDescription: some View {
+        VStack (spacing: 16) {
+            Text("Todos os Pokémons em um só Lugar")
+                .font(Font.custom("Poppins-Medium", size: 26))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color("Primary"))
+            // (.multilineTextAlignment)Alinhar textos com mais de uma linha
+            Text("Acesse uma vasta lista de Pokémon de todas as gerações já feitas pela Nintendo")
+                .font(Font.custom("Poppins-Regular", size: 14))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color("Secondary"))
+        }
+    }
+    
+    var onboardingProgressView: some View {
+        HStack {
+            Rectangle()
+                .frame(width: 28, height: 9)
+                .clipShape(.capsule)
+                .foregroundColor(Color("DarkBlue"))
+            Circle()
+                .frame(width: 9, height: 9)
+                .foregroundStyle(Color(.lightGray))
+        }
+    }
+    
+    var continueButton: some View {
+        Button(action: {
+            
+        }, label: {
+            Rectangle()
+                .frame(height: 58)
+                .clipShape(.capsule)
+                .foregroundColor(Color("DarkBlue"))
+                .overlay {
+                    //overlay e sendo usado para colocar encima ou na frente
+                    Text("Continuar")
+                        .foregroundStyle(.white)
+                        .font(Font.custom("Poppins-SemiBold", size: 18))
+                }
+        })
     }
 }
 
