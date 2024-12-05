7787571a300aaf1b9877ac76bb7d2db5b930ffa0
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-12-05 16:33:13 -0300

    insert shadowTrainer3

--- MyPokedexSwiftUI/ContentView.swift
+++ MyPokedexSwiftUI/ContentView.swift
@@ -8,76 +8,104 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel: OnboardingViewModel
     var body: some View {
-        VStack {
-            trainersImages
-            Spacer().frame(height: 45)
-            
-            titleAndDescription
-            Spacer().frame(height: 24)
-            
-            onboardingProgressView
-            Spacer().frame(height: 24)
-            
-            continueButton
-            
+        TabView(selection: $viewModel.currentStep) {
+            ForEach(0..<viewModel.onboardingSteps.count, id: \.self) {
+                index in
+                VStack {
+                    trainersImages
+                    Spacer().frame(height: 45)
+                    titleAndDescription(title: viewModel.onboardingSteps[index].title, description: viewModel.onboardingSteps[index].description)
+                    Spacer().frame(height: 24)
+                    onboardingProgressView
+                    Spacer().frame(height: 24)
+                    continueButton(buttonText: viewModel.onboardingSteps[index].buttonText)
+                    
+                }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
+                       maxHeight: .infinity,
+                       alignment: .bottom)
+            .padding()
+            }
         }
-        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-               maxHeight: .infinity,
-               alignment: .bottom)
-        .padding()
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
     }
     
+    @ViewBuilder
     var trainersImages: some View {
-        // Utilizando a ZSstack para sobrepor as imagens.
-        ZStack {
-            Image("trainer1 1")
-            // Com o (.offset) pode se mudar os eixos com x e y.
-                .offset(x: -50)
-            //Background pode se colocar imagens atras.
-                .background {
-                    Image("shadowTrainer1")
-                        .offset(x: -70, y: 120)
-                }
-            Image("trainer2 1")
-                .offset(x: 50, y: -10)
+        if viewModel.currentStep == 0 {
+            // Utilizando a ZSstack para sobrepor as imagens.
+            ZStack {
+                Image("trainer1")
+                // Com o (.offset) pode se mudar os eixos com x e y.
+                    .offset(x: -50)
+                //Background pode se colocar imagens atras.
+                    .background {
+                        Image("shadowTrainer1")
+                            .offset(x: -70, y: 120)
+                    }
+                Image("trainer2")
+                    .offset(x: 50, y: -10)
+                    .background{
+                        Image("shadowTrainer2")
+                            .offset(x: 50, y: 115)
+                    }
+                // Eixo X e o orizontal e o Y e vertical.
+            }
+        } else {
+            Image("trainer3")
                 .background{
-                    Image("shadowTrainer2")
-                        .offset(x: 50, y: 115)
+                    Image("shadowTrainer3")
+                        .offset(x: -10,y: 128)
                 }
-            // Eixo X e o orizontal e o Y e vertical.
         }
+
     }
     
-    var titleAndDescription: some View {
+    func titleAndDescription(title: String, description: String) -> some View {
         VStack (spacing: 16) {
-            Text("Todos os Pokémons em um só Lugar")
+            Text(title)
                 .font(Font.custom("Poppins-Medium", size: 26))
                 .multilineTextAlignment(.center)
                 .foregroundStyle(Color("Primary"))
             // (.multilineTextAlignment)Alinhar textos com mais de uma linha
-            Text("Acesse uma vasta lista de Pokémon de todas as gerações já feitas pela Nintendo")
+            Text(description)
                 .font(Font.custom("Poppins-Regular", size: 14))
                 .multilineTextAlignment(.center)
                 .foregroundStyle(Color("Secondary"))
         }
     }
     
+    @ViewBuilder
     var onboardingProgressView: some View {
-        HStack {
-            Rectangle()
-                .frame(width: 28, height: 9)
-                .clipShape(.capsule)
-                .foregroundColor(Color("DarkBlue"))
-            Circle()
-                .frame(width: 9, height: 9)
-                .foregroundStyle(Color(.lightGray))
+        if viewModel.currentStep == 0 {
+            HStack {
+                Rectangle()
+                    .frame(width: 28, height: 9)
+                    .clipShape(.capsule)
+                    .foregroundColor(Color("DarkBlue"))
+                Circle()
+                    .frame(width: 9, height: 9)
+                    .foregroundStyle(Color(.lightGray))
+            }
+        } else {
+            HStack {
+                Circle()
+                    .frame(width: 9, height: 9)
+                    .foregroundStyle(Color(.lightGray))
+                Rectangle()
+                    .frame(width: 28, height: 9)
+                    .clipShape(.capsule)
+                    .foregroundColor(Color("DarkBlue"))
+            }
         }
+        
     }
     
-    var continueButton: some View {
+    func continueButton(buttonText: String) -> some View {
         Button(action: {
-            
+            viewModel.currentStep = 1
         }, label: {
             Rectangle()
                 .frame(height: 58)
@@ -85,7 +113,7 @@ struct ContentView: View {
                 .foregroundColor(Color("DarkBlue"))
                 .overlay {
                     //overlay e sendo usado para colocar encima ou na frente
-                    Text("Continuar")
+                    Text(buttonText)
                         .foregroundStyle(.white)
                         .font(Font.custom("Poppins-SemiBold", size: 18))
                 }
@@ -94,5 +122,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView(viewModel: OnboardingViewModel())
 }
